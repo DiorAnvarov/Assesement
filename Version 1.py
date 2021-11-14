159872e57b58413a5c8dc8c7bf13d35c9af983e3
Dior Anvarov <Anvarov.dior00@gmail.com>
2021-11-14 17:26:53 +0000

    Error handling added

--- Version 1.py
+++ Version 1.py
@@ -2,12 +2,14 @@ import random
 class RandNumbers:
     def __init__(self):
 
-        self.limit = int(input("How many numbers should be returned?: "))
-        self.small = int(input("Enter the lowest number: "))
-        self.big = int(input("Enter the highest number: "))
-        self.retrive(self.limit, self.small, self.big)
+        try:
+            self.limit = int(input("How many numbers should be returned?: "))
+            self.small = int(input("Enter the lowest number: "))
+            self.big = int(input("Enter the highest number: "))
+            self.retrive(self.limit, self.small, self.big)
 
-
+        except:
+            print("Error try again")
 
     def retrive(self, limit,small,big):
         for i in range(limit):
